99546798d6cc92e11f1b9315e1f9d4df90e1c71e
RudeGames <fs_boukhoulda@esi.dz>
2018-07-10 15:35:07 +0100

    Created models and migrated to database

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password','avatar'
     ];
 
     /**
@@ -26,4 +26,13 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+
+    public function discussion(){
+        return $this->hasMany('App\Discussion');
+    }
+
+    public function replies(){
+        return $this->hasMany('App\Reply');
+    }
 }
