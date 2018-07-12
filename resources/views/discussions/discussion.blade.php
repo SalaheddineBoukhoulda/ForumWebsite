cb4a87c4e13d6bea1e0a885cad19c56c954bc59b
RudeGames <fs_boukhoulda@esi.dz>
2018-07-12 21:30:29 +0100

    Added discussion view with replies

--- resources/views/discussions/discussion.blade.php
+++ resources/views/discussions/discussion.blade.php
@@ -1,12 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-            <div class="card">
-                <div class="card-header">{{$discussion->title}}</div>
-
-                <div class="card-body">
-
-                    You are logged in!
-                </div>
+<div class="card">
+        <div class="card-header">
+            <img src="{{asset($discussion->user->avatar)}}" alt="{{$discussion->title}}" width="40px" height="40px">
+            &nbsp;&nbsp;
+            <span>{{$discussion->user->name}}</span>
+        </div>
+        <div class="card-body">
+            <div class="text-center">
+                  <h4><b>{{$discussion->title}}</b></h4>
             </div>
+            <hr>
+            {{$discussion->content}}
+        </div>
+        <div class="card-footer">
+            {{$discussion->replies->count()}} Replies
+        </div>
+</div>
+<br>
+<br>
+@foreach($discussion->replies as $reply)
+<div class="card">
+        <div class="card-header">
+                <img src="{{asset($reply->user->avatar)}}" alt="{{$reply->user->name}}" width="40px" height="40px">
+                &nbsp;&nbsp;
+                <span>{{$reply->user->name}}</span>
+        </div>
+        <div class="card-body">
+                {{$reply->content}}
+        </div>
+        <div class="card-footer">
+                Like
+        </div>
+</div>
+@endforeach
 @endsection
